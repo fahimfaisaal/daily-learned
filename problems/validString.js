7015c5f60142aa16b4e22bd3ca833847f303dc3b
Fahim Faisaal <fahimfaisal.soikot@gmail.com>
2022-11-06 08:34:28 +0600

    chore: fix naming

--- problems/validString.js
+++ problems/validString.js
@@ -8,14 +8,14 @@ const strings = [
 
 /**
  * initialize a Stack & Map with key left bracket & value right bracket
- * traverse each bracket of the braces
+ * traverse each bracket of the brackets
  * check is it exist in map or not
  * if exist push it in the stack (cause it's left bracket) 
  * if not exist then check is this bracket is equal to last left bracket on the stack
  * if match then remove it from the stack
  * if not match then return false
  */
-const isValidString = (braces) => { 
+const isValidBrackets = (brackets) => { 
   const stack = new Stack()
   const bracketMap = {
     '(': ')',
@@ -23,11 +23,12 @@ const isValidString = (braces) => {
     '[': ']'
   }
 
-  for (const bracket of braces) {
-    if (bracket in bracketMap) {
-      stack.push(bracket)
+  for (const curBracket of brackets) {
+    if (curBracket in bracketMap) {
+      stack.push(curBracket)
     } else {
-      if (bracket === bracketMap[stack.top()]) {
+      const rightBracket = bracketMap[stack.top()]
+      if (curBracket === rightBracket) {
         stack.pop() 
       } else {
         return false
@@ -39,7 +40,7 @@ const isValidString = (braces) => {
 }
 
 const solutions = strings.reduce((acc, cur) => {
-  acc[cur] = isValidString(cur)
+  acc[cur] = isValidBrackets(cur)
   return acc
 }, {})
 
