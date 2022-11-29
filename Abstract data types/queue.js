c257c9db045dd31dd9b316a7d814d0d1f4b69eea
Developer <fahimfaisal.soikot@gmail.com>
2022-11-29 09:28:39 +0600

    feat(ds): implement queue data structure

---
+++ Abstract data types/queue.js
@@ -0,0 +1,68 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  #length;
+  constructor(initialValues = []) {
+    this.#length = 0;
+    this.head = null;
+
+    for (const value of initialValues) {
+      this.enqueue(value);
+    }
+  }
+
+  get size() {
+    return this.#length;
+  }
+
+  peekFirst() {
+    return this.head?.value;
+  }
+
+  peekLast() {
+    return this.tail?.value;
+  }
+
+  enqueue(data) {
+    const node = new Node(data);
+
+    if (this.head === null) {
+      this.head = this.tail = node;
+    } else {
+      this.tail.next = node;
+      this.tail = node;
+    }
+
+    return this;
+  }
+
+  dequeue() {
+    if (this.head) {
+      const value = this.head.value;
+      this.head = this.head.next;
+
+      if (this.head === null) {
+        this.tail = null;
+      }
+      return value;
+    }
+
+    return null;
+  }
+}
+
+const queue = new Queue([1, 2, 3, 4]);
+console.log("dequeue", queue.dequeue());
+console.log("dequeue", queue.dequeue());
+console.log("dequeue", queue.dequeue());
+console.log("dequeue", queue.dequeue());
+console.log("dequeue", queue.dequeue());
+
+console.log(queue);
+
+module.exports = Queue;
